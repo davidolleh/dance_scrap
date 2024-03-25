9cf96020881465c8a2be16a8738cec19c7b0c15d
davidolleh <asherolleh@gmail.com>
2024-03-25 15:33:45 +0900

    [feat] item 저장시 사용할 pipeline spider 마다 생성

---
+++ tutorial/pipelines.py
@@ -0,0 +1,32 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class YGXPipeline(object):
+    def open_spider(self, spider):
+        spider.logger.info('Tutorial Spider Pipeline Started')
+        # self.file = open("items_19.json", "w")
+
+    def process_item(self, item, spider):
+        return item
+
+    def close_spider(self, spider):
+        spider.logger.info('Tutorial Spider Pipeline closed')
+
+
+class OneMillionPipeline(object):
+
+    def open_spider(self, spider):
+        spider.logger.info('Tutorial Spider Pipeline Started')
+
+    def process_item(self, item, spider):
+        return item
+
+    def close_spider(self, spider):
+        spider.logger.info('Tutorial Spider Pipeline closed')
